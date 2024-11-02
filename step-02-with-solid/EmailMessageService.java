b372d12aa62cb927e20945f8def1803ed209a3cb
smmsajjadi <93717646+smmsajjadi@users.noreply.github.com>
2024-11-02 20:43:49 +0330

    Update EmailMessageService.java

--- step-02-with-solid/EmailMessageService.java
+++ step-02-with-solid/EmailMessageService.java
@@ -1,33 +1,21 @@
 import java.util.regex.Pattern;
 
-public class EmailMessageService implements MessageService {
+public class EmailMessageService implements MessageService<EmailMessage> {
     @Override
-    public void sendSmsMessage(SmsMessage smsMessage) {
-        //Empty Body
-    }
-
-    @Override
-    public void sendEmailMessage(EmailMessage emailMessage) {
-        if(validateEmailAddress(emailMessage.getSourceEmailAddress()) && validateEmailAddress(emailMessage.getTargetEmailAddress())){
-            System.out.println("Sending a SMS from " + emailMessage.getSourceEmailAddress() + " to " + emailMessage.getTargetEmailAddress() + " with content : " + emailMessage.getContent());
-        }else{
-            throw new IllegalArgumentException("Email Address is Not Correct!");
+    public void sendMessage(EmailMessage emailMessage) {
+        if (validateEmailAddress(emailMessage.getSourceEmailAddress()) &&
+            validateEmailAddress(emailMessage.getTargetEmailAddress())) {
+            System.out.println("Sending an email from " + emailMessage.getSourceEmailAddress() + 
+                               " to " + emailMessage.getTargetEmailAddress() +
+                               " with content: " + emailMessage.getContent());
+        } else {
+            throw new IllegalArgumentException("Email Address is not correct!");
         }
     }
 
-    @Override
-    public void sendTelegramMessage(TelegramMessage telegramMessage) {
-        //Empty Body
-    }
-
-    public boolean validateEmailAddress(String email) {
-        // Regular expression pattern for validating email addresses
+    private boolean validateEmailAddress(String email) {
         String emailRegex = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
-
-        // Compile the pattern into a regex Pattern object
         Pattern pattern = Pattern.compile(emailRegex);
-
-        // Check if the email string matches the regex pattern
         return pattern.matcher(email).matches();
     }
 }
