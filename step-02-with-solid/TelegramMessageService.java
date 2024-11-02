e2d5a570746d309e198c589dae4cca153df932e3
smmsajjadi <93717646+smmsajjadi@users.noreply.github.com>
2024-11-02 20:47:55 +0330

    Update TelegramMessageService.java

--- step-02-with-solid/TelegramMessageService.java
+++ step-02-with-solid/TelegramMessageService.java
@@ -1,31 +1,21 @@
-public class TelegramMessageService implements MessageService {
-    @Override
-    public void sendSmsMessage(SmsMessage smsMessage) {
-        //Empty Body
-    }
+import java.util.regex.Pattern;
 
+public class TelegramMessageService implements MessageService<TelegramMessage> {
     @Override
-    public void sendEmailMessage(EmailMessage emailMessage) {
-        //Empty Body
-    }
-
-    @Override
-    public void sendTelegramMessage(TelegramMessage telegramMessage) {
-        if(validateID(telegramMessage.getSourceID()) && validateID(telegramMessage.getTargetID())){
-            System.out.println("Sending a telegram message from" + telegramMessage.getSourceID() + " to " + telegramMessage.getTargetID() + " with content : " + telegramMessage.getContent());
-        }else{
+    public void sendMessage(TelegramMessage telegramMessage) {
+        if (validateID(telegramMessage.getSourceID()) &&
+            validateID(telegramMessage.getTargetID())) {
+            System.out.println("Sending a telegram message from " + telegramMessage.getSourceID() +
+                               " to " + telegramMessage.getTargetID() +
+                               " with content: " + telegramMessage.getContent());
+        } else {
             throw new IllegalArgumentException("Telegram ID is not correct!");
         }
     }
 
-    public boolean validateID(String id) {
-        // Regular expression pattern for validating ID
-        String idRegex = "\w{4,}$";
-
-        // Compile the pattern into a regex Pattern object
+    private boolean validateID(String id) {
+        String idRegex = "\\w{4,}$";
         Pattern pattern = Pattern.compile(idRegex);
-
-        // Check if the email string matches the regex pattern
         return pattern.matcher(id).matches();
     }
 }
